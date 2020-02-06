69c797de94ca7fcbac90e4ddb8961cf72aebf347
Kyle Whittemore <kyle.whittemore@gmail.com>
2020-02-06 18:52:07 -0500

    changed api to post to a test board

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,8 @@ router.post('/api/card', (req, res) => {
     console.log('Request name: ', name);
     console.log('Request description', description)
     
-    const queryUrl = `${baseUrl}cards?name=${name}&desc=${description}&pos=top&idList=5e33039423688f1f1ec37cce&keepFromSource=all&key=${process.env.API_KEY}&token=${process.env.TOKEN}`
+    // const queryUrl = `${baseUrl}cards?name=${name}&desc=${description}&pos=top&idList=5e33039423688f1f1ec37cce&keepFromSource=all&key=${process.env.API_KEY}&token=${process.env.TOKEN}`
+    const queryUrl = `${baseUrl}cards?name=${name}&desc=${description}&pos=top&idList=5e3ca5ae070ad821902976ae&keepFromSource=all&key=${process.env.API_KEY}&token=${process.env.TOKEN}`
 
     Axios.post(queryUrl)
     .then(response => {
